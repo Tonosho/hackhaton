45ac8841395b508ae94467863e3c77e78154b616
Tonosho <102292299+Tonosho@users.noreply.github.com>
2023-03-31 07:56:56 +0200

    chart implemetend + cleaned data flow

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const moment = require('moment');
+const router = express.Router();
 
 const temperatureModel = require('../models/temperature')
 
@@ -9,37 +10,54 @@ router.get('/test', async (req, res) => {
   res.json({ test });
 });
 
-// MONGOOSE POST TEST
-router.post('/addTemp', async (req, res) => {
-  let newTemp = await new temperatureModel({
-    Type: req.body.Type,
-    Date: req.body.Date,
-    Temp: req.body.Temp,
-  })
-  let tempSaved = await newTemp.save();
-  res.json({ tempSaved });
+// LOGIN CHECK
+router.post('/login', async (req, res) => {
+  let userPassword = req.body.password;
+  if (userPassword === "test") {
+    res.json({ validation: 'success' })
+  }
+  else {
+    res.json({ validation: 'error' });
+  }
 })
-
 
 // GET CLOSEST TEMPERATURES FOR DASHBOARD
 router.get('/closestTemperatures', async (req, res) => {
-  let temperatures = await temperatureModel.find()
-
+  const allTemperatures = await temperatureModel.find()
   const now = moment();
-  const closest = temperatures.reduce((a, b) => {
-    const adiff = a.Date - today;
-    return adiff > 0 && adiff < b.Date - today ? a : b;
+
+  const roomTemperatures = allTemperatures.filter(temp => temp.Type === "roomTemp")
+
+  const closestRoomTemp = roomTemperatures.reduce((a, b) => {
+    const accumDate = moment(a.Date);
+    const currentDate = moment(b.Date);
+    if (currentDate > now) {
+      return a; // Ignore values from future
+    }
+    return accumDate.diff(now) > currentDate.diff(now) ? a : b;
   });
 
-  res.json({ closestTemperatures })
+  const outsideTemperatures = allTemperatures.filter(temp => temp.Type === "outsideTemp")
+
+  const closestOutsideTemp = outsideTemperatures.reduce((a, b) => {
+    const accumDate = moment(a.Date);
+    const currentDate = moment(b.Date);
+    if (currentDate > now) {
+      return a; // Ignore dates from future
+    }
+    return accumDate.diff(now) > currentDate.diff(now) ? a : b;
+  });
+
+  res.json({ room: closestRoomTemp, outside: closestOutsideTemp })
 })
 
-
-
-
+// BRING ALL TEMPERATURES BY TYPE
 router.get('/allTemperatures', async (req, res) => {
-  let allTemperatures = await temperatureModel.find()
-  res.json({ allTemperatures })
+
+  const roomTemperatures = await temperatureModel.find({ Type: "roomTemp" })
+  const outsideTemperatures = await temperatureModel.find({ Type: "outsideTemp" })
+
+  res.json({ room: roomTemperatures, outside: outsideTemperatures })
 })
 
 module.exports = router;
